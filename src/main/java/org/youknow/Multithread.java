9e7b3ed3e894493b03696a1821d6f12ec3f6d1aa
vermisse <vermisse@qq.com>
2018-10-17 14:18:20 +0800

    优化顺序

--- src/main/java/org/youknow/Multithread.java
+++ src/main/java/org/youknow/Multithread.java
@@ -5,6 +5,7 @@ import java.net.*;
 import java.util.*;
 
 import org.jsoup.*;
+import org.jsoup.Connection.*;
 import org.jsoup.nodes.*;
 import org.jsoup.select.*;
 
@@ -18,11 +19,13 @@ public class Multithread implements Runnable {
 	public String url;
 	
 	public static void main(String[] args) {
+		// 这里写一个就开一个新线程
 		String[] arr = {
 			"http://7y8k.com/?m=vod-type-id-5.html",
 			"http://7y8k.com/?m=vod-type-id-6.html",
 			"http://7y8k.com/?m=vod-type-id-7.html",
 			"http://7y8k.com/?m=vod-type-id-8.html",
+			"http://7y8k.com/?m=vod-type-id-9.html",
 		};
 		
 		for(String url : arr) {
@@ -39,33 +42,36 @@ public class Multithread implements Runnable {
 
 		o: while (true) {
 			cache = new ArrayList<String>();
+
+			boolean off = false;
+			
 			for (String page : list) {
 				try {
 					Document doc = Jsoup.connect(page).get();
-					Elements a = doc.select(".l h2 a");
-				
-					Iterator<Element> tag = a.iterator();
-					while (tag.hasNext()) {
-						Element lk = tag.next();
-						String url = lk.attr("abs:href");
-						
-						System.out.println(url);
-						
-						level2(url);
-					}
 					
 					Elements next = doc.select(".page .pagelink_a");
 					Iterator<Element> it = next.iterator();
-
-					boolean off = false;
 					
 					while (it.hasNext()) {
 						Element link = it.next();
 						String text = link.text();
 						if (text.equals("下一页")) {
-							System.out.println(link.attr("abs:href"));
+							System.out.println("发现：" + link.attr("abs:href"));
 							cache.add(link.attr("abs:href"));
+							
+							off = true;
 						}
+					}
+					
+					Elements a = doc.select(".l h2 a");
+					Iterator<Element> tag = a.iterator();
+					while (tag.hasNext()) {
+						Element lk = tag.next();
+						String url = lk.attr("abs:href");
+						
+						System.out.println("当前下载：" + url);
+						
+						level2(url);
 					}
 
 					if(!off)
@@ -81,7 +87,7 @@ public class Multithread implements Runnable {
 	public void level2(String url) throws Exception {
 		Document doc = Jsoup.connect(url).get();
 		Elements a = doc.select("#vlink_1 ul li a");
-		System.out.println(a.attr("abs:href"));
+		System.out.println("分析地址：" + a.attr("abs:href"));
 		
 		doc = Jsoup.connect(a.attr("abs:href")).get();
 		String html = doc.toString();
@@ -92,8 +98,8 @@ public class Multithread implements Runnable {
 		if(index != -1 && index < 4)
 			name = name.substring(index + 1);
 			
-		System.out.println(name);
-		System.out.println(URLDecoder.decode(source, "UTF-8"));
+		System.out.println("文件名：" + name);
+		System.out.println("文件路径：" + URLDecoder.decode(source, "UTF-8"));
 		
 		downVideo(basePath + name, URLDecoder.decode(source, "UTF-8"));
 	}
@@ -111,7 +117,6 @@ public class Multithread implements Runnable {
 		// 编码之后的url
 		videoUrl = beforeUrl + newFileName;
 		
-		InputStream is = null;
 		FileOutputStream out = null;
 		
 		try {
@@ -120,12 +125,7 @@ public class Multithread implements Runnable {
 			if (!files.exists()) {
 				files.mkdirs();
 			}
-			// 获取下载地址
-			URL url = new URL(videoUrl);
-			// 链接网络地址
-			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-			// 获取链接的输出流
-			is = connection.getInputStream();
+			
 			// 创建文件，fileName为编码之前的文件名
 			File file = new File(filePath + fileName);
 			if(file.exists())
@@ -133,21 +133,18 @@ public class Multithread implements Runnable {
 			
 			file = new File(filePath + fileName + ".tmp");
 			if(file.exists())
-				return;
+				file.delete();
+
+			Response res = Jsoup.connect(videoUrl).ignoreContentType(true).execute();
 			
 			// 根据输入流写入文件
 			out = new FileOutputStream(file);
-			int i = 0;
-			while ((i = is.read()) != -1) {
-				out.write(i);
-			}
+			out.write(res.bodyAsBytes());
 			out.close();
 			
 			file.renameTo(new File(filePath + fileName));
 		} catch (Exception e) {
 			e.printStackTrace();
-		} finally {
-			is.close();
 		}
 	}
 }
